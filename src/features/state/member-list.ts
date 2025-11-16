6542bda194a30d8429d15584e19fc82a3413ccab
William Bowen <william@progress.win>
2025-11-16 18:56:04 -0500

    fix: formatting

--- src/features/state/member-list.ts
+++ src/features/state/member-list.ts
@@ -1,23 +1,24 @@
-import { AttachmentBuilder, ChatInputCommandInteraction, MessageFlags } from 'discord.js';
-import { ns } from '../../commands/chat/state.js';
-import { localize } from '../../i18n.js';
+import { AttachmentBuilder, ChatInputCommandInteraction, MessageFlags } from "discord.js";
+import { ns } from "../../commands/chat/state.js";
+import { localize } from "../../i18n.js";
 
 /**
  * Executes a chat input command interaction to export role members to a CSV file.
  * @param interaction - The chat input command interaction object.
  */
 export async function memberList(interaction: ChatInputCommandInteraction) {
+	
 	// Defer the reply to indicate that the bot is processing the command.
 	await interaction.deferReply({ flags: MessageFlags.Ephemeral });
 	
 	if(!interaction.inCachedGuild()) return
 	
 	// Extract the locale and options from the interaction.
-	const local = localize.getLocale(interaction.locale);
+	const localeBundle = localize.getLocale(interaction.locale);
 	const options = interaction.options
 
 	// Get the role from interaction options using true to make the argument required
-	const role = options.getRole('role', true)
+	const role = options.getRole("role", true)
 
 	// Create a CSV attachment using the AttachmentBuilder class.
 	const csv = new AttachmentBuilder(
@@ -25,14 +26,14 @@ export async function memberList(interaction: ChatInputCommandInteraction) {
 		Buffer.from(`Display Name,Username,Id\n${
 			role.members.map((member) =>
 				`${member.displayName},${member.user.username},${member.id}`
-		).join('\n')}`),
+		).join("\n")}`),
 		// Set the file name for the CSV attachment based on the role name and interaction ID.
-		{ name: `${role.name.replace(' ', '-')}.csv` }
+		{ name: `${role.name.replace(" ", "-")}.csv` }
 	);
 
   // Send a follow-up message with a content and the CSV file attached.
   await interaction.editReply({
-    content: local?.t("member-list-message-followup", ns, {
+    content: localeBundle?.t("member-list-message-followup", ns, {
     	role: role.toString(),
     }),
     files: [csv],
