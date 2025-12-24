021b6d401a41ea625d5776b18767b1fb90953768
William Bowen <bowenjw22@outlook.com>
2025-12-23 22:32:29 -0500

    rewrite pvapi handler

---
+++ src/Classes/API/Warn.ts
@@ -0,0 +1,36 @@
+import { ApiConnService } from "./ApiConnService/ApiConnService.js";
+import { warn } from "./ApiConnService/types.js";
+
+export class Warn {
+  data: warn;
+  api: ApiConnService;
+
+  constructor(api: ApiConnService, data: warn) {
+    this.data = data;
+    this.api = api;
+  }
+
+  get reason() {
+    return this.data.reason;
+  }
+
+  get targetId() {
+    return this.data.userWarnedDiscordId;
+  }
+
+  get moderatorId() {
+    return this.data.moderatorDiscordId;
+  }
+
+  get createdAt() {
+    return new Date(this.data.createdAtUtc);
+  }
+
+  get expiresAt() {
+    return new Date(this.data.expiresAtUtc);
+  }
+
+  get updatedAt() {
+    return new Date(this.data.updatedAtUtc);
+  }
+}
---
+++ src/commands/chat/moderation/create.ts
@@ -0,0 +1,5 @@
+// import { ChatInputCommandInteraction } from "discord.js";
+
+// export async function create(interaction:ChatInputCommandInteraction) {
+
+// }
