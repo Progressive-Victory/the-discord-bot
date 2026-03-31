0f5330b1f6239b9b41112a183584544593d8a7ab
Marcus Doucette <Marz3587@gmail.com>
2026-03-31 15:01:32 -0400

    add JSDocs to ScheduledEventWrapper class

--- src/util/scheduledEventWrapper.ts
+++ src/util/scheduledEventWrapper.ts
@@ -3,9 +3,17 @@ import { createObjectCsvWriter } from "csv-writer";
 import { GuildMember, GuildScheduledEventStatus, time } from "discord.js";
 import { client } from "..";
 
+/**
+ * Wrapper class for "DiscordEvent" 
+ * Meant to provide easier access and some useful utility functions
+ */
 export class ScheduledEventWrapper {
   event: DiscordEvent;
 
+  /**
+   * Utility function for getting a color representation of the event's status
+   * @returns blue for completed; green for active; red for canceled; white for undefined
+   */
   statusColor = () => {
     let color: number;
     switch (this.event.status) {
@@ -28,6 +36,10 @@ export class ScheduledEventWrapper {
     return color;
   };
 
+  /**
+   * Utility function for getting the duration of the event
+   * @returns time in minutes that an event went on; "N/A" if event hasn't finished yet.
+   */
   duration = () => {
     if (!this.event.startedAtUtc) {
       return "N/A";
@@ -45,98 +57,174 @@ export class ScheduledEventWrapper {
     }
   };
 
+  /**
+   * Utility function to fetch guild id for this bot (defined in env file)
+   * @returns guild id
+   */
   guild = async () => {
     if (!process.env.PV_GUILD_ID)
       throw Error("fill out 'PV_GUILD_ID' in env file");
     return await client.guilds.fetch(process.env.PV_GUILD_ID);
   };
 
+  /**
+   * Utility function to get up to date DiscordEvent from this ScheduledEventWrapper
+   * @returns DiscordEvent
+   */
   guildEvent = async () => {
     return await (
       await this.guild()
     ).scheduledEvents.fetch(this.event.discordId);
   };
 
+  /**
+   * Utility fucntion to find this event's channel
+   * @returns GuildBasedChannel
+   */
   channel = async () => {
     return this.event.channelId
       ? await (await this.guild()).channels.fetch(this.event.channelId)
       : null;
   };
 
+  /**
+   * Utility function for fetching the time this event was created
+   * @returns string representation of time created
+   */
   createdAt = () => {
     return time(this.event.createdAtUtc);
   };
 
+  /**
+   * Utility function to fetch event description
+   * @returns DiscordEvent.description
+   */
   description = () => {
     return this.event.description ? this.event.description : "None";
   };
 
+  /**
+   * Utility fucntion to fetch the member who created this event
+   * @returns Guildmember who created this event
+   */
   creator = async () => {
     return (await this.guild()).members.fetch(this.event.creatorDiscordId);
   };
 
+  /**
+   * Utility function to fetch time when event ended
+   * @returns string representation of time ended or "None" if the event hasn't ended
+   */
   scheduledEnd = () => {
     return this.event.scheduledEndUtc
       ? time(this.event.scheduledEndUtc)
       : "None";
   };
 
+  /**
+   * Utility function to fetch current scheduled start time
+   * @returns string representation of start time or "None" if the event hasn't been scheduled
+   */
   scheduledStart = () => {
     return this.event.scheduledStartUtc
       ? time(this.event.scheduledStartUtc)
       : "None";
   };
 
+  /**
+   * Utility function to fetch current scheduled start date
+   * @returns string representation of start date or "None" if the event hasn't been scheduled
+   */
   scheduledStartDate = () => {
     return this.event.scheduledStartUtc
       ? time(this.event.scheduledStartUtc, "D")
       : "None";
   };
 
+  /**
+   * Utility function to fetch the time of day when this event is planned to start
+   * @returns string representation of time of day for event start or "None" if the event hasn't been scheduled
+   */
   scheduledStartTime = () => {
     return this.event.scheduledStartUtc
       ? time(this.event.scheduledStartUtc, "t")
       : "None";
   };
 
+  /**
+   * Utility function to fech the time of day the event is scheduled to end
+   * @returns string representation of the time of day for this event to end or "None" if the event's end hasn't been scheduled
+   */
   scheduledEndTime = () => {
     return this.event.scheduledEndUtc
       ? time(this.event.scheduledEndUtc, "t")
       : "None";
   };
 
+  /**
+   * Utility function to fetch the date that this event started
+   * @returns string representation of the date this event started or "None" if this event hasn't begun
+   */
   startDate = () => {
     return this.event.startedAtUtc
       ? time(this.event.startedAtUtc, "D")
       : "None";
   };
 
+  /**
+   * Utility fucntion to fetch the time of day when this event was started
+   * @returns string representation of time of day when this event started or "None" if this event hasn't begun
+   */
   startTime = () => {
     return this.event.startedAtUtc
       ? time(this.event.startedAtUtc, "t")
       : "None";
   };
 
+  /**
+   * Utility function to fetch the time of day when this event was ended
+   * @returns string representation of the time of day when this event ended or "None" if this event hasn't ended
+   */
   endTime = () => {
     return this.event.endedAtUtc ? time(this.event.endedAtUtc, "t") : "None";
   };
 
+  /**
+   * Utility function for fetcing the event's name
+   * @returns DiscordEvent.name
+   */
   name = () => {
     return this.event.name;
   };
 
+  /**
+   * Utility function for fetching this event's status
+   * @returns GuildScheduledEventStatus
+   */
   status = () => {
     return GuildScheduledEventStatus[this.event.status ?? 1];
   };
 
+  /**
+   * Utility function to fetch date and time when this event started
+   * @returns string representation of event start time or "N/A" if event hasn't started
+   */
   startedAt = () => {
     return this.event.startedAtUtc ? time(this.event.startedAtUtc) : "N/A";
   };
 
+  /**
+   * Utility function to fetch date and time when this event ended
+   * @returns string representation of event end time or "N/A" if event hasn't ended
+   */
   endedAt = () => {
     return this.event.endedAtUtc ? time(this.event.endedAtUtc) : "N/A";
   };
 
+  /**
+   * Utility function to fetch a list of join and leave events formatted as strings
+   * @returns string[] of all user join and leave events
+   */
   attendees = () => {
     if (!this.event.attendees)
       throw Error("No attendees defined on event: " + this.event.id);
@@ -150,24 +238,44 @@ export class ScheduledEventWrapper {
     return users;
   };
 
+  /**
+   * Utility function to fetch the event user count
+   * @returns DiscordEvent.userCount
+   */
   userCount = () => {
     return this.event.userCount;
   };
 
+  /**
+   * Utility function to fetch information about if this event is recurring
+   * @returns "Recurring" or "One Time"
+   */
   recurrence = () => {
     return this.event.recurrent ? "Recurring" : "One Time";
   };
 
+  /**
+   * Utility function to fetch the thumbnail of this event
+   * @returns DiscordEvent.thumbnailUrl
+   */
   thumbnail = () => {
     return this.event.thumbnailUrl;
   };
 
+  /**
+   * Utility function to fetch a url to this event
+   * @returns GuildScheduledEvent.url
+   */
   eventLink = async () => {
     const guild = await this.guild();
     const res = await guild.scheduledEvents.fetch(this.event.discordId);
     return res.url;
   };
 
+  /**
+   * Utility function to get a list of user join and leave events with their usernames instead of discord ids
+   * @returns string[] of user join and leave events
+   */
   attendeesNames = async () => {
     if (!this.event.attendees)
       throw Error("No attendees defined on event: " + this.event.id);
@@ -180,6 +288,10 @@ export class ScheduledEventWrapper {
     return this.populateNames(entries, nameMap);
   };
 
+  /**
+   * Utility function to fetch the number of unique users who joined this event
+   * @returns number of attendees 
+   */
   uniqueAttendees = () => {
     if (!this.event.attendees)
       throw Error("No attendees defined on event: " + this.event.id);
@@ -190,6 +302,10 @@ export class ScheduledEventWrapper {
     return usrIds.length;
   };
 
+  /**
+   * Utility function to fetch the percentage of the event each user was present for
+   * @returns string[] of userids and their attendance percentages
+   */
   attendancePercentages = () => {
     const users: string[] = [];
     this.calculateAttendancePercentages()?.forEach(
@@ -201,10 +317,17 @@ export class ScheduledEventWrapper {
     return users;
   };
 
+  /**
+   * Make a new ScheduledEventWrapper from a DiscordEvent
+   * @param ev source discord event
+   */
   constructor(ev: DiscordEvent) {
     this.event = ev;
   }
 
+  /**
+   * Utility function to dump event attendance details into a csv file "./assets/temp/attendees.csv"
+   */
   public async writeCsvDump() {
     if (!this.event.attendees)
       throw Error("No attendees defined on event: " + this.event.id);
@@ -234,6 +357,12 @@ export class ScheduledEventWrapper {
     console.log("csv written");
   }
 
+  /**
+   * private function for replacing occurrances of <@'id'> with a users username
+   * @param entries list of strings that need this replacement (see ScheduledEventWrapper.attendees())
+   * @param nameMap map keyed with ids to fetch names (see ScheduledEventWrapper.getAtendeeNames())
+   * @returns new list of entries where <@'id'> is replaced with users names 
+   */
   private populateNames(entries: string[], nameMap: Map<string, string>) {
     return entries.map((entry) => {
       const id = entry.slice(2, 20);
@@ -242,11 +371,21 @@ export class ScheduledEventWrapper {
     });
   }
 
+  /**
+   * private function for custom time formatting
+   * @param time "Date" object for time to be formatted
+   * @returns properly formatted string representation of given time
+   */
   private getFormattedTime(time: Date) {
     const tzOffset = time.getTimezoneOffset() / 60;
     return `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()} UTC${tzOffset < 0 ? "-" : "+"}${tzOffset}`;
   }
 
+  /**
+   * private function to fetch atendees usernames based on their guild nickname
+   * @param ids list of user ids
+   * @returns map for getting a users name from their id
+   */
   private async getAttendeeNames(ids: string[]) {
     const buffer = [];
     const names: Map<string, string> = new Map();
@@ -273,6 +412,10 @@ export class ScheduledEventWrapper {
     return names;
   }
 
+  /**
+   * private function for calculating user attendance to an event
+   * @returns map of total time a user attended this event, keyed by their user id
+   */
   private calculateAttendanceTime() {
     interface joinLeavePair {
       userDiscordId: string;
@@ -340,6 +483,10 @@ export class ScheduledEventWrapper {
     }
   }
 
+  /**
+   * private function for finding the total percentage of the event any given attendee was present for
+   * @returns map of attendence percentages, keyed by user id
+   */
   private calculateAttendancePercentages() {
     try {
       if (!this.event.endedAtUtc || !this.event.startedAtUtc)
