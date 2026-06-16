56299983e83351401fddd948e841d5e896bcfa82
William Bowen <bowenjw22@outlook.com>
2026-06-15 21:10:22 -0400

    remove feedback command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,7 +1,5 @@
 export { default as state } from "./chat/state";
 
-export { default as feedback } from "./chat/feedback";
-
 export { warn } from "./chat/moderation";
 
 // export { userProfile } from "./context_menu/profile";
