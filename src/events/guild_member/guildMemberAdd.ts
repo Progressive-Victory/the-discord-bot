b92b805582f0c287b915b5e648736a91c62fe732
ultrafeeder4 <ultrafeeder4@gmail.com>
2026-06-25 21:22:46 -0400

    added users route and a try/catch to guildMemberAdd.ts for user join event

--- src/events/guild_member/guildMemberAdd.ts
+++ src/events/guild_member/guildMemberAdd.ts
@@ -1,5 +1,8 @@
+import { apiConnService } from "@/util/api/pvapi";
 import Event from "@/Classes/Event";
 import { Events } from "discord.js";
+import { Routes } from "@/Classes/API/ApiConnService/routes";
+import { zCreateUserRequest } from "@/contracts/requests/CreateUserRequest";
 
 /**
  * `guildMemberAdd` handles the {@link Events.GuildMemberAdd} {@link Event}. Currently,
@@ -12,5 +15,10 @@ export const guildMemberAdd = new Event({
       Events.Debug,
       `user ${member.id} "${member.user.username}"  joined at ${member.joinedTimestamp}`,
     );
+    try {
+      await apiConnService.post(Routes.users, null);
+    } catch (e) {
+      console.error(e);
+    }
   },
 });
