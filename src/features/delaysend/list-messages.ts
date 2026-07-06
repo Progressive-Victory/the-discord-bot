0c3049c94c16f6430be86b6cc41f34070d44c7d5
Luke Norman <60233616+LNorman8@users.noreply.github.com>
2026-07-06 13:33:39 -0600

    Listing now works properly (for the usecase).
    I don't know how I would render actually complex component messages (outside of just having Discord do it). Nor do I know a way to just get the data as a JSON dump or something. But this works for now

--- src/features/delaysend/list-messages.ts
+++ src/features/delaysend/list-messages.ts
@@ -1,11 +1,45 @@
 import {
+  ActionRowData,
+  APIMessageTopLevelComponent,
   bold,
   ChatInputCommandInteraction,
   codeBlock,
+  ComponentType,
+  isJSONEncodable,
+  JSONEncodable,
+  MessageActionRowComponentBuilder,
+  MessageActionRowComponentData,
   MessageFlags,
+  TextDisplayComponentData,
+  TopLevelComponentData,
   unorderedList,
 } from "discord.js";
 import { scheduledMessages } from "../../util/delaysend/state.js";
+
+// Drill down into the component and extract its string representation
+function componentToStr(
+  component:
+    | JSONEncodable<APIMessageTopLevelComponent>
+    | TopLevelComponentData
+    | ActionRowData<
+        MessageActionRowComponentData | MessageActionRowComponentBuilder
+      >
+    | APIMessageTopLevelComponent,
+) {
+  if (isJSONEncodable(component)) {
+    // Returns the contained value (not a JSON string as I assumed)
+    component = component.toJSON();
+  }
+  switch (component.type) {
+    case ComponentType.TextDisplay:
+      return (component as TextDisplayComponentData).content;
+    //TODO: We will want a way to show any Components message, not just ones with TextDisplays. 
+    // Might need to just send each in a separate ephemeral message so Discord renders? 
+    default:
+      throw "Unimplemented component type";
+  }
+}
+
 export async function listScheduledMessages(
   command: ChatInputCommandInteraction,
 ) {
@@ -23,7 +57,7 @@ export async function listScheduledMessages(
       .toArray()
       .map(
         (msg) =>
-          `${bold(msg.id)} (${msg.pattern})\n${codeBlock(msg.payload.components?.join(", ") ?? "Unknown")}`,
+          `${bold(msg.id)} (${msg.pattern})\n${codeBlock(msg.payload.components?.map(componentToStr).join("\n\n") ?? "Unknown")}`,
       ),
   );
   console.debug(messageList);
