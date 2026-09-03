66bc3574b0a60ba7c9a7d183425abc03829e6196
zoe2276 <zoe@zlynn.dev>
2026-09-03 16:25:06 +0000

    address feedback from 1459bee

--- src/features/logging/scheduledEvent.ts
+++ src/features/logging/scheduledEvent.ts
@@ -68,10 +68,7 @@ export async function logScheduledEvent(event: DiscordEvent, init: boolean) {
     if (existingPost) {
       const { cont } = await logContainer(event, init);
       const files = [];
-      if (
-        !event.thumbnailUrl ||
-        event.thumbnailUrl === "attachment://image.jpg"
-      )
+      if (!event.thumbnailUrl || event.thumbnailUrl === FALLBACK_IMAGE)
         files.push(new AttachmentBuilder("./assets/image.jpg"));
       if (!init)
         files.push(new AttachmentBuilder("./assets/temp/attendees.csv"));
@@ -85,10 +82,7 @@ export async function logScheduledEvent(event: DiscordEvent, init: boolean) {
     } else {
       const { cont } = await logContainer(event, init);
       const files = [];
-      if (
-        !event.thumbnailUrl ||
-        event.thumbnailUrl === "attachment://image.jpg"
-      )
+      if (!event.thumbnailUrl || event.thumbnailUrl === FALLBACK_IMAGE)
         files.push(new AttachmentBuilder("./assets/image.jpg"));
       if (!init)
         files.push(new AttachmentBuilder("./assets/temp/attendees.csv"));
